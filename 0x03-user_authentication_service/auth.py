6ec54f1f85eb44ab91977fb6c6abda9af5245439
Sark_oo7 <kosinick01@gmail.com>
2024-04-24 22:06:03 +0100

    10. Get session ID

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -51,3 +51,15 @@ class Auth:
             return bcrypt.checkpw(provided_pswd_bytes, user.hashed_password)
         except NoResultFound:
             return False
+
+    def create_session(self, email: str) -> str:
+        """It takes an email string argument
+        and returns the session ID as a string.
+        """
+        try:
+            user = self._db.find_user_by(email=email)
+            session_id = _generate_uuid()
+            self._db.update_user(user.id, session_id=session_id)
+            return session_id
+        except Exception:
+            return None
