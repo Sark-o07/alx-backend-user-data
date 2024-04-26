64eb956e075b13f0b65d95f8f6f2e61e00a779ec
Sark_oo7 <kosinick01@gmail.com>
2024-04-26 11:26:42 +0100

    15. User profile

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -59,7 +59,14 @@ def logout():
 
 @app.route('/profile', methods=['DELETE'], strict_slashes=False)
 def profile():
-    pass
+    """
+    Display User Information
+    """
+    session_id = request.cookies.get("session_id")
+    user = AUTH.get_user_from_session_id(session_id)
+    if user:
+        return jsonify({"email": user.email}), 200
+    abort(403)
 
 
 if __name__ == "__main__":
